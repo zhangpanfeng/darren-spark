b0b3c679c86bbe14f65bd225d1ea0894ca19c8de
Darren Zhang <sf0902@163.com>
2018-12-04 22:49:20 +0800

    update document

--- src/main/scala/com/darren/test/Test.scala
+++ src/main/scala/com/darren/test/Test.scala
@@ -1,11 +1,19 @@
 package com.darren.test
 
-object Test{
+import org.apache.spark.sql.SparkSession
+
+object Test {
   def main(args: Array[String]): Unit = {
-    println("hello world")
+    val spark = SparkSession.builder().master("local").appName("test").getOrCreate()
+    val text = spark.sparkContext.textFile("test-in/load/txt/EventDB-DDL.sql")
 
-    new Test1
-
+    //text.persist()
+    for(i <- 1 to 3){
+      val start = System.currentTimeMillis();
+      println(text.count())
+      println("Cost: " + (System.currentTimeMillis() - start))
+    }
+    spark.close()
   }
 
 
